0912f092f9df3560574d3f2b3237be5849ce4e60
sigit11 <sigitsetiaji4@gmail.com>
2017-09-26 14:41:47 +0700

    parallax

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,24 @@
+
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
     event.preventDefault();
     $(this).ekkoLightbox();
 });
+
+
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
+
+// var jumboHeight = $('.download-section').outerHeight();
+// function parallax(){
+//     var scrolled = $(window).scrollTop();
+//     $('.bg-1').css('height', (jumboHeight-scrolled) + 'px');
+// }
+
+// $(window).scroll(function(e){
+//     parallax();
+// });
